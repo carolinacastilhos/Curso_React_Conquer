d8eb5590c0331bb9ca9c4f6a7875524a048ed672
Carolina Castilhos <carolinacastilhos.vet@gmail.com>
2023-08-07 00:05:20 -0300

    ending Filmz

--- filmz/src/pages/Movie.tsx
+++ filmz/src/pages/Movie.tsx
@@ -1,58 +1,58 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { FiCameraOff, FiCheck, FiLink, FiPlus } from "react-icons/fi"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { FiCameraOff, FiCheck, FiLink, FiPlus } from "react-icons/fi";
 
-import { Button } from "../components/Button"
-import { ButtonVariants } from "../components/Button/types"
-import { Loading } from "../components/Loading"
+import { Button } from "../components/Button";
+import { ButtonVariants } from "../components/Button/types";
+import { Loading } from "../components/Loading";
 
-import { useWishlist } from "../hooks/useWishlist"
+import { useWishlist } from "../hooks/useWishlist";
 
-import api from "../services/api"
+import api from "../services/api";
 
-import { IMovieRequestProps } from "../interface/Movie"
+import { IMovieRequestProps } from "../interface/Movie";
 
-import * as Styles from "../styles/pages/Movie"
+import * as Styles from "../styles/pages/Movie";
 
 export function Movie() {
-  const { id } = useParams()
-  const { isMovieInWishlist, handleAddOrRemoveMovieOnWishlist } = useWishlist()
+  const { id } = useParams();
+  const { isMovieInWishlist, handleAddOrRemoveMovieOnWishlist } = useWishlist();
 
-  const currentUrl = window.location.href
+  const currentUrl = window.location.href;
 
-  const [isLoading, setIsLoading] = useState(true)
-  const [movie, setMovie] = useState<IMovieRequestProps>()
-  const [isLinkCopiedToClipboard, setIsLinkCopiedToClipboard] = useState(false)
+  const [isLoading, setIsLoading] = useState(true);
+  const [movie, setMovie] = useState<IMovieRequestProps>();
+  const [isLinkCopiedToClipboard, setIsLinkCopiedToClipboard] = useState(false);
 
-  const movieYearRelease = movie && new Date(movie.release_date).getFullYear()
-  const movieHours = movie && Number(Math.trunc(movie.runtime / 60).toFixed(0))
+  const movieYearRelease = movie && new Date(movie.release_date).getFullYear();
+  const movieHours = movie && Number(Math.trunc(movie.runtime / 60).toFixed(0));
   const movieMinutes =
-    movie && movieHours && (movie.runtime - movieHours * 60).toFixed(0)
+    movie && movieHours && (movie.runtime - movieHours * 60).toFixed(0);
 
   function handleCopyLinkToClipboard() {
-    setIsLinkCopiedToClipboard(true)
+    setIsLinkCopiedToClipboard(true);
 
-    navigator.clipboard.writeText(currentUrl)
+    navigator.clipboard.writeText(currentUrl);
   }
 
   useEffect(() => {
     api
       .get<IMovieRequestProps>(`/movie/${id}`)
       .then((response) => {
-        setMovie(response.data)
+        setMovie(response.data);
       })
       .finally(() => {
-        setIsLoading(false)
-      })
-  }, [])
+        setIsLoading(false);
+      });
+  }, []);
 
   useEffect(() => {
     if (isLinkCopiedToClipboard) {
       setTimeout(() => {
-        setIsLinkCopiedToClipboard(false)
-      }, 2500)
+        setIsLinkCopiedToClipboard(false);
+      }, 2500);
     }
-  }, [isLinkCopiedToClipboard])
+  }, [isLinkCopiedToClipboard]);
 
   return (
     <Styles.Container>
@@ -146,5 +146,5 @@ export function Movie() {
         </section>
       )}
     </Styles.Container>
-  )
+  );
 }
